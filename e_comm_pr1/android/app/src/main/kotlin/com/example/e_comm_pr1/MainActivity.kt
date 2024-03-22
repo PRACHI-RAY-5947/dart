1549918dd1a7107243e21b1dd21dfbd315950d69
PRACHI-RAY-5947 <149695789+PRACHI-RAY-5947@users.noreply.github.com>
2024-03-22 11:12:22 +0530

    Delete e_comm_pr1 directory

--- e_comm_pr1/android/app/src/main/kotlin/com/example/e_comm_pr1/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.e_comm_pr1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- e_comm_pr1/lib/utils/routes.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class MyRoutes {
-  static String homePage = '/';
-  static String detailPage = "detailPage";
-  static String cartPage = "cartPage";
-}
